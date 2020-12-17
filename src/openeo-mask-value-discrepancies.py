94ac9d39c17df57ad2da8fcdf9ebabeb05e3fb54
fdetsch <fdetsch@web.de>
2020-12-17 15:56:16 +0100

    update: apply mask correctly.

--- src/openeo-mask-value-discrepancies.py
+++ src/openeo-mask-value-discrepancies.py
@@ -1,16 +1,28 @@
+### ENVIRONMENT ====
+
+### . modules ----
+
 import openeo
-import rasterio
+
+import georaster
+import matplotlib.pyplot as plt
+
 import numpy as np
+
+
+### . openeo ----
 
 connection = openeo.connect("https://openeo.vito.be").authenticate_basic("test", "test123")
 
+
+### PROCESSING ====
 
 ### . ndwi ----
 
 sentinel2_data_cube = connection.load_collection(
   "SENTINEL2_L2A_SENTINELHUB"
   , spatial_extent = {'west':5.151836872,'east':5.1533818244,'south':51.181925592,'north':51.184696360,'crs':4326}
-  , temporal_extent = ["2020-05-01", "2020-05-10"]
+  , temporal_extent = ["2020-05-02", "2020-05-02"]
   , bands = ['B08','B12']
 )
 
@@ -20,10 +32,13 @@ B12 = sentinel2_data_cube.band('B12')
 ndwi_cube = (B08 - B12) / (B08 + B12)
 ndwi_cube.download("data/ndwi.tif", format = "GTiff")
 
-## value range
-img = rasterio.open("data/ndwi.tif")
-[img.read(1).min(), img.read(1).max()]
-# [-0.26342073, 0.6292848]
+# Use SingleBandRaster() if image has only one band
+img = georaster.SingleBandRaster('data/ndwi.tif')
+# img.r gives the raster in [height, width, band] format 
+# band no. starts from 0
+plt.imshow(img.r)
+plt.show()
+plt.clf()
 
 
 ### . scl ----
@@ -31,18 +46,22 @@ img = rasterio.open("data/ndwi.tif")
 s2_scl = connection.load_collection(
   "SENTINEL2_L2A_SENTINELHUB"
   , spatial_extent = {'west':5.151836872,'east':5.1533818244,'south':51.181925592,'north':51.184696360,'crs':4326}
-  , temporal_extent = ["2020-05-01", "2020-05-10"]
+  , temporal_extent = ["2020-05-02", "2020-05-02"]
   , bands = ['SCL']
 )
 
 mask = s2_scl.band('SCL')
+mask.download("data/scl.tif", format = "GTiff")
+
 mask = mask != 4
 
 ndwi_cube_masked = ndwi_cube.mask(mask) # .resample_cube_spatial(ndwi_cube)
 ndwi_cube_masked.download("data/ndwi_masked.tif", format = "GTiff")
 
-img1 = rasterio.open("data/ndwi_masked.tif")
+img1 = georaster.SingleBandRaster('data/ndwi_masked.tif')
+plt.imshow(img1.r)
+plt.show()
+plt.clf()
 
-## value range
-[np.nanmin(img1.read(1)), np.nanmax(img1.read(1))]
-# [-0.06359782, 0.703763]
+georaster.MultiBandRaster
+img - img1
